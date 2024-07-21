63656a24cbd70e94749584de359f0131eed3ed2a
Abdallah Awad <abdallah.awad@medcor.com>
2024-07-21 10:29:10 +0300

    Add project files.

---
+++ TrainingAPi/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingAPi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult GetUserName()
+        {
+            return new OkObjectResult("Abdallah");
+        }
+
+
+        [HttpGet("GetUser")]
+        public IActionResult GetUser()
+        {
+            return new OkObjectResult("Abdallah");
+        }
+
+
+    }
+}
